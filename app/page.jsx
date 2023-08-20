734c981e69ad9c196685add1d49443fbac053b52
dalmalipcsei <hello@dalmart.co.uk>
2023-08-20 19:08:13 +0100

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation
+      </p>
+
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
